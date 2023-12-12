8f372852b8a3a92a4384ea1d881dfc8fb78ddcd0
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-12 19:55:04 +0100

    Initial commit

--- uniones.cpp
+++ uniones.cpp
@@ -20,7 +20,7 @@ int main() {
     miVariable.cadena = new std::string("Hola, mundo!");
     std::cout << "Valor cadena: " << *(miVariable.cadena) << std::endl;
 
-    // Liberar la memoria asignada dinámicame
+    // Liberar la memoria asignada dinámica
     delete miVariable.cadena;
 
     return 0;
